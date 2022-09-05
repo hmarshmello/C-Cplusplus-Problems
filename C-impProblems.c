e3fc6a8c3f409ba16ba1aed9abd8d9322f2dfc05
Harsh Shrivastava <62792682+hmarshmello@users.noreply.github.com>
2022-09-05 09:40:59 +0530

    Questions

---
+++ C-impProblems.c
@@ -0,0 +1,10 @@
+// 1.Stacks using an array
+// 2.stacks using LL
+// 3.Applications of stacks
+// 4.Find the max in stacks in O(1)
+// 5.Find the largest area of the rectangle which is inscribed in the histogram
+// 6.Next greater element
+// 7.Dutch national flag problem
+// 8.Lead an element in an array
+// 9.Find the missing and the repeated number(Microsoft)
+// 10.Grid unique path\
