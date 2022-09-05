3f136b6b593186dfa392b50be38ecb64ccc33206
Harsh Shrivastava <62792682+hmarshmello@users.noreply.github.com>
2022-09-05 13:08:04 +0530

    update

--- C-impProblems.c
+++ C-impProblems.c
@@ -2,9 +2,14 @@
 // 2.stacks using LL
 // 3.Applications of stacks
 // 4.Find the max in stacks in O(1)
-// 5.Find the largest area of the rectangle which is inscribed in the histogram
+// 5.Find the largest area of the rectangle which is inscribed in the histogram // ip 2 1 5 6 2 3
 // 6.Next greater element
 // 7.Dutch national flag problem
 // 8.Lead an element in an array
 // 9.Find the missing and the repeated number(Microsoft)
 // 10.Grid unique path\
+
+ 
+// 5. (right smaller index - left smaller index+1 )x a[i]
+//// usin stack optimised way
+///// 
