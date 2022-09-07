7609301d40b1d1de405658e305936a84677bd29d
Harsh Shrivastava <62792682+hmarshmello@users.noreply.github.com>
2022-09-07 08:48:13 +0530

    delete

--- Google-BS.c
+++
@@ -1,8 +0,0 @@
-// Google as well as in Flipkart
-// Allocate Books
-// Given an array of intergers A of size N,
-// College library has N books, ith book has a[i] number of pages,
-// you have to allocate books to B number of students so that
-// max number of pages alloted to student in Minimum.
-// IP: 12 34 67 90, B = 90
-// OP: 113
